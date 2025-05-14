9e2fcc734b2e38f984d3e0c3ee7039eca2fb8f19
junaid014 <junaidhossen014@gmail.com>
2025-05-14 18:18:48 +0600

    add header footer and hero section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+ 
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import HeroSection from './HeroSection'
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <HeroSection/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
