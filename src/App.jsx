586a7e809932c15c044a5e12c986bbaa7dc31bbf
junaid014 <junaidhossen014@gmail.com>
2025-05-15 12:36:25 +0600

    add taskbord sections

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
      <Header/>
-     <div className='lg:px-20'>
+     <div className='flex flex-col justify-center items-center'>
       <HeroSection/>
      <TaskBoard/>
      </div>
